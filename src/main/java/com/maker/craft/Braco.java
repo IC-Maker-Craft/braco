53d977c4b84a549aa7b9e9f4b9f5c9506e39f249
Henrique Soubhia <soubhiahenrique0@gmail.com>
2025-07-01 12:27:38 -0300

    communication servo mine

---
+++ src/main/java/com/maker/craft/Braco.java
@@ -0,0 +1,4 @@
+package com.maker.craft;
+
+public class Braco {
+}
--- src/main/java/com/maker/craft/MakerCraft.java
+++ src/main/java/com/maker/craft/MakerCraft.java
@@ -1,41 +1,56 @@
 package com.maker.craft;
 
+import com.fazecast.jSerialComm.SerialPort;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.network.message.SentMessage;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
-import java.util.function.Supplier;
+
 
 public class MakerCraft implements ModInitializer {
 
-	int braco1value = 0;
+
+	Integer braco1value = 0;
+
+	SerialPort comPort;
 
 	@Override
 	public void onInitialize() {
+
+		comPort = Arduino.Connect();
+
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			dispatcher.register(CommandManager.literal("braco")
 					.then(CommandManager.argument("id", IntegerArgumentType.integer(1))
 							.then(CommandManager.literal("add")
-									.executes(ctx -> executarBraco(ctx, true)))
+									.then(CommandManager.argument("valor", IntegerArgumentType.integer(1))
+										.executes(ctx -> executarBraco(ctx, true))))
 							.then(CommandManager.literal("remove")
-									.executes(ctx -> executarBraco(ctx, false)))
+									.then(CommandManager.argument("valor", IntegerArgumentType.integer(1))
+										.executes(ctx -> executarBraco(ctx, false))))
 					));
 		});
+
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			Arduino.close();
+		}));
 	}
 
 	private int executarBraco(CommandContext<ServerCommandSource> context, boolean adicionar) throws CommandSyntaxException {
 		int id = IntegerArgumentType.getInteger(context, "id");
+		int valor = IntegerArgumentType.getInteger(context, "valor");
 
-		if(adicionar && braco1value < 180){
-			braco1value += 1;
+
+		if(adicionar && braco1value < 120){
+			braco1value += valor;
 		}else if(!adicionar && braco1value > 0){
-			braco1value -= 1;
+			braco1value -= valor;
 		}
 
 		context.getSource().getServer().getPlayerManager().broadcast(
@@ -43,6 +58,8 @@ public class MakerCraft implements ModInitializer {
 				false
 		);
 
+		Arduino.sendSerialMessage(comPort, braco1value.toString());
+		
 
 		// Aqui você pode chamar sua lógica: exemplo, ArduinoController.setBraco(id, adicionar)
 
